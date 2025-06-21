f01f2622c549263888ec92702c78b3fb9e308dc8
KanishkSingh0801 <kanishksinghjatav@gmail.com>
2025-06-21 14:24:53 +0530

    Lottery Game

--- board-game/vite-project/src/App.jsx
+++ board-game/vite-project/src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
-import Lottery from './Lottery';
-import LudoBoard from './LudoBoard';
-import TodoList from './TodoList';
+import "./App.css";
+import Lottery from "./Lottery";
+import LudoBoard from "./LudoBoard";
+import TicketNum from "./TicketNum";
+import TodoList from "./TodoList";
+import Ticket from "./Ticket";
 
 function App() {
-
   return (
     <>
-    {/* <LudoBoard/> */}
-    {/* <TodoList/> */}
-    <Lottery/>
+      <Lottery n={3} winningSum = {15}></Lottery>
     </>
   );
 }
 
-export default App
+export default App;
--- board-game/vite-project/src/Lottery.jsx
+++ board-game/vite-project/src/Lottery.jsx
@@ -1,39 +1,62 @@
+// import { useState } from "react";
+
+// export default function Lottery() {
+//   let [number, setNumber] = useState(0);
+//   let winner = 15;
+//   let [userNumber, setUserNumber] = useState(0);
+//   let [message, setMessage] = useState("");
+
+//   function sumOfDigit() {
+//     let num = Math.floor(Math.random() * 1000) + 1;
+//     console.log(num);
+//     setUserNumber(num);
+//     return num
+//       .toString()
+//       .split("")
+//       .reduce((sum, digit) => sum + parseInt(digit), 0);
+//   }
+
+//   let generateNumber = () => {
+
+//     let num = sumOfDigit();
+//     console.log("sum = " + num);
+//     if (num == winner) {
+//       setMessage("'Congratulations, you won!' ");
+//       return true;
+//     } else {
+//         setMessage("");
+//       return false;
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Lottery {message}</h1>
+//       <h2>Lottery ticket = {userNumber}</h2>
+//       <button onClick={generateNumber}>Get New Ticket</button>
+//     </div>
+//   );
+// }
+import React from "react";
 import { useState } from "react";
+import { genTicket, sum } from "./Helper";
+import Ticket from "./Ticket";
 
-export default function Lottery() {
-  let [number, setNumber] = useState(0);
-  let winner = 15;
-  let [userNumber, setUserNumber] = useState(0);
-  let [message, setMessage] = useState("");
 
-  function sumOfDigit() {
-    let num = Math.floor(Math.random() * 1000) + 1;
-    console.log(num);
-    setUserNumber(num);
-    return num
-      .toString()
-      .split("")
-      .reduce((sum, digit) => sum + parseInt(digit), 0);
+export default function Lottery({n = 3, winningSum = 15}) {
+  let [ticket, setTicket] = useState(genTicket(n));
+  let isWinning = sum(ticket) === winningSum;
+
+  let buyTicket = () => {
+    setTicket(genTicket(n));
   }
-
-  let generateNumber = () => {
-    
-    let num = sumOfDigit();
-    console.log("sum = " + num);
-    if (num == winner) {
-      setMessage("'Congratulations, you won!' ");
-      return true;
-    } else {
-        setMessage("");
-      return false;
-    }
-  };
-
   return (
     <div>
-      <h1>Lottery {message}</h1>
-      <h2>Lottery ticket = {userNumber}</h2>
-      <button onClick={generateNumber}>Get New Ticket</button>
+      <h1>Lottery Game!</h1>
+      <Ticket ticket={ticket}/>
+      <br></br>
+      <button onClick={buyTicket}>Buy new ticket</button>
+      <h3> {isWinning && "Congratulations, you won !!"}</h3>
     </div>
   );
 }
